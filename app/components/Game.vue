72662ab83bb0aeb629355a78ee00b619cd169f31
unknown <alexandre.mary.adenni@gmail.com>
2019-05-23 17:45:37 +0200

    update

--- app/components/Game.vue
+++ app/components/Game.vue
@@ -8,11 +8,26 @@
         <router-link v-if="button2" class="button button2" :to="route2">{{ button2 }}</router-link>
         <router-link v-if="button3 && show" class="button button3" :to="route3">{{ button3 }}</router-link>
         <router-link v-if="button4" class="button4" :to="route1"><button class="button" @click="map">{{ button4 }}</button></router-link>
+        <router-link v-if="button5" class="button5" :to="route1"><button class="button" @click="tresor">{{ button5 }}</button></router-link>
+        <router-link v-if="button6 && showE" class="button button6" :to="route4">{{ button6 }}</router-link>
       </div>
-      <audio src="" autoplay refs="audio"></audio>
+    </div>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <div id="audio">
+      <button class="button pauseMusic" v-on:click="play"><i class="fa fa-pause"></i></button>
+      <audio src="../assets/monkey-island-the-scummbar-ost.mp3" autoplay loop refs="audioElm"></audio>
     </div>
   </div>
 </template>
+
+<style>
+
+.pauseMusic {
+  position: absolute;
+  bottom: -45vh;
+  left: -45vw;
+}
+</style>
 
 
 <script>
@@ -21,6 +36,7 @@ import countService from '../json/countServices';
 import saveBoussole from '../json/saveBoussole.js';
 import saveEpee from '../json/saveEpee.js';
 import saveMap from '../json/saveMap.js';
+import saveTresor from '../json/saveTresor.js';
 
 export default {
   computed: {
@@ -31,6 +47,9 @@ export default {
       return `${data[this.$route.params.id].choix2}`;
     },
     route3() {
+      return `${data[this.$route.params.id].choix3}`;
+    },
+    route4() {
       return `${data[this.$route.params.id].choix3}`;
     },
     message() {
@@ -48,22 +67,54 @@ export default {
     button4() {
       return `${data[this.$route.params.id].button4}`;
     },
+    button5() {
+      return `${data[this.$route.params.id].button5}`;
+    },
+    button6() {
+      return `${data[this.$route.params.id].button6}`;
+    },
     background() {
       return `${data[this.$route.params.id].img}`;
     },
     show() {
       return localStorage.getItem('outil') === 'boussole'
     },
+    showE() {
+      return localStorage.getItem('outil') === 'epee'
+    }
   },
-  mounted() {
-    setTimeout(() => {
-      this.$refs.audio.pause();
-    }, 1000);
-  },
+  // mounted() {
+  //   setTimeout(() => {
+  //     this.$refs.audio.pause();
+  //   }, 1000);
+  // },
   methods: {
     map() {
       saveMap.save();
+      if (localStorage.getItem('item') === 'carte' && localStorage.getItem('item2') === 'tresor') {
+        this.$router.push('../win');
+        location.reload();
+        localStorage.clear();
+      }
     },
+    tresor() {
+      saveTresor.save();
+      if (localStorage.getItem('item') === 'carte' && localStorage.getItem('item2') === 'tresor') {
+        this.$router.push('../win');
+        location.reload();
+        localStorage.clear();
+      }
+    },
+    //marche pas
+    play: function(event) {
+      var a = this.$refs.audioElm;
+      console.log(this.$refs.audioElm);
+      if (a.paused) {
+        a.play();
+      } else {
+        a.pause();
+      }
+    }
   }
 };
 </script>
--- app/components/Win.vue
+++ app/components/Win.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="big-header">
-    <h1>Page</h1>
-    <br />
-    <router-link class="button" to="/">Go to Home</router-link>
+  <div class="big-header big-header--losewin">
+    <h1 class="title--win">Victoire, vous êtes désormais un pirate !</h1>
+    <video src="../assets/video.mp4" autoplay muted loop></video>
+    <audio src="../assets/victoire.mp3" autoplay loop></audio>
+    <router-link class="button" to="/">Recommencer</router-link>
   </div>
 </template>
+
+<style>
+video {
+  position: absolute;
+  z-index: -1;
+}
+</style>
 
 <script>
 export default {
